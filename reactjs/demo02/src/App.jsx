d93ed80bed1d2fe52a1d211411103a95bf0907fb
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-06-30 16:16:54 +0900

    demo02 기본 설정

--- reactjs/demo02/src/App.jsx
+++ reactjs/demo02/src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  //절대규칙 : 화면은 무조건 하나로 묶어서 반환
   return (
-    <h1>KH정보교육원</h1>
+    <>
+      <h1>KH정보교육원</h1>
+      <p>저희 학원에 오신것을 환영합니다!</p>
+    </>
   )
 }
 
