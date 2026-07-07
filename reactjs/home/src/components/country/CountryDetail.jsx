43dbf2ac2e037a8e333c6deffdc9eb4a8168dd9b
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-07-07 17:45:46 +0900

    국가 상세페이지에서 국가명 수정가능한 상태로 변경

--- reactjs/home/src/components/country/CountryDetail.jsx
+++ reactjs/home/src/components/country/CountryDetail.jsx
@@ -3,8 +3,8 @@ import Jumbotron from "../../templates/Jumbotron";
 import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { Button, Col, Row } from "react-bootstrap";
-import { FaList, FaPenToSquare, FaTrash } from "react-icons/fa6";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { FaCheck, FaList, FaPenToSquare, FaSquarePen, FaTrash, FaXmark } from "react-icons/fa6";
 import Swal from "sweetalert2";
 
 export default function CountryDetail() {
@@ -32,6 +32,7 @@ export default function CountryDetail() {
     const loadData = useCallback(async ()=>{
         const response = await axios.get(`http://localhost:8080/api/country/${countryNo}`)
         setCountry(response.data);
+        setBackup(response.data);
     }, []);
 
     const deleteCountry = useCallback(async () => {
@@ -53,6 +54,36 @@ export default function CountryDetail() {
         navigate("/country/list");
     }, [countryNo]);
 
+    //수정을 구현하기 위해서 논리형 state와 백업용 state 구현
+    const [backup, setBackup] =useState(null);
+    const [editMode, setEditMode] = useState({
+        countryName:false,
+        countryCapital:false,
+        countryRegion:false,
+        countryPopulation:false,
+    });
+
+    const changeStringValue = useCallback(e=>{
+        const {name, value} = e.target;
+        setCountry({
+            ...country,
+            [name] : value
+        });
+    })
+
+    //국가명만 변경하는 함수
+    const updateCountryName = useCallback(async ()=>{
+        const response = await axios.patch(
+            `http://localhost:8080/api/country/${countryNo}`, 
+            {countryName : country.countryName}
+        );
+        //백업을 갱신
+        setBackup({...backup, countryName:country.countryName});
+        //수정모드를 취소
+        setEditMode({...editMode, countryName:false});
+        toast.success("국가 변경이 완료되었습니다");
+    }, [country, backup, editMode]);
+
     return (<>
         <Jumbotron title="국가 상세 정보" content={`${countryNo}번 국가의 상세 정보 화면입니다`} />
 
@@ -65,7 +96,26 @@ export default function CountryDetail() {
                     국가명
                 </Col>
                 <Col sm={9}>
-                    {country.countryName}
+                    {editMode.countryName !== true ? (<>
+
+                        <span>{country.countryName}</span>
+                        <FaSquarePen className="text-warning ms-2"
+                                onClick={e=>{
+                                    setEditMode({...editMode, countryName : true})
+                                }}/>
+                    </>) : (<>
+                        <Form.Control type="text" className="w-auto d-inline-block"
+                            name="countryName" value={country.countryName}
+                            onChange={changeStringValue}/>
+                        <FaCheck className="text-success ms-2"
+                                onClick={updateCountryName}/>
+                        <FaXmark className="text-danger ms-2" 
+                                onClick={e=>{
+                                    setCountry({...country, countryName: backup.countryName})
+                                    setEditMode({...editMode, countryName : false})
+                                }}/>
+                    </>)}
+                    
                 </Col>
                 <Col sm={3} className="text-info fw-bold">
                     대륙
