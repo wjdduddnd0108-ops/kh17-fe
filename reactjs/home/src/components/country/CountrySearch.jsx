d912714385504d0b9cf2ee6c075004bfd4a731cb
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-07-10 17:22:06 +0900

    검색어 자동완성

--- reactjs/home/src/components/country/CountrySearch.jsx
+++ reactjs/home/src/components/country/CountrySearch.jsx
@@ -2,51 +2,39 @@ import Jumbotron from "@templates/Jumbotron";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { Col, Form, ListGroup, Row } from "react-bootstrap";
+import { throttle, debounce } from "lodash-es";
 
-export default function CountrySearch(){
+export default function CountrySearch() {
     //state
     const [keyword, setKeyword] = useState("");
     const [searchList, setSearchList] = useState([]);
-    const [composition, setComposition] = useState(false);//입력 글자가 조합중인지 상태값
-
-    //입력중인 값을 제거한 검색용 키워드
-    const [result, setResult] = useState("");
 
     //callback
-    const changeKeyword = useCallback(e=>{
-        //e.data가 존재하면 onCompositionUpdate 상황, 없으면 onChange 상황
-        if(e.data !== undefined){//onCompositionUpdate 상황이면 e.data를 확인해서 keyword를 갱신
-            // console.log(e.target.value, e.data);
-            setResult(e.target.value.substring(0, e.target.value.length-1));
-        }
-        else{//onChange 상황 (기존처럼 keyword를 업데이트)
-            setKeyword(e.target.value);
-        }
+    const changeKeyword = useCallback(e => {
+        setKeyword(e.target.value);
     }, []);
 
-    useEffect(()=>{
-        searchKeyword();
-    }, [result]);
-
     //effect
-    // - keyword가 변하면 ajax 요청을 서버로 전송
-    // useEffect(()=>{
-    //     searchKeyword();
-    // }, [keyword]);
+    //- keyword가 변하면 ajax 요청을 서버로 전송
+    useEffect(() => {
+        searchKeyword(keyword);
+    }, [keyword]);
 
-    const searchKeyword = useCallback(async()=>{
-        console.log(composition, keyword);
-         if(result.length === 0) {
+    //throttle(함수, 실행주기) => 새로운 함수가 생성됨
+    //- (주의) 함수를 만들 때 연관항목을 설정하지 말아야 한다 (함수가 재생성이 안되어야 함)
+    //- 일반적으로 실행주기는 250ms ~ 350ms 정도가 적당 (1초에 3~4번)
+    const searchKeyword = useCallback(throttle(async (keyword)=> {
+        console.log("searchKeyword 실행");
+        if(keyword.length === 0) {
             setSearchList([]);
             return;
         }
-
-        const response = await axios.get(`/api/country/countryName/${result}`);
+        const response = await axios.get(`/api/country/countryName/${keyword}`);
         setSearchList(response.data);
-    },[result, composition]);
+    }, 350), []);
 
-    return(<>
-        <Jumbotron title="국가명 검색 샘플"/>
+    return (<>
+        <Jumbotron title="국가명 검색 샘플" />
 
         {/* 검색창 */}
         <Row className="mt-4">
@@ -54,18 +42,12 @@ export default function CountrySearch(){
                 <div className="position-relative">
                     <Form.Control placeholder="검색어 입력" size="lg"
                         value={keyword}
-                        onChange={changeKeyword}
-                        onCompositionStart={e=>setComposition(true)}
-                        onCompositionUpdate={changeKeyword}
-                        onCompositionEnd={e=>{
-                            setResult(e.target.value);
-                            setComposition(false)
-                        }}/>
+                        onChange={changeKeyword} />
                     <ListGroup className="position-absolute start-0 end-0 top-100">
-                        {searchList.map(country=>(
-                        <ListGroup.Item key={country.countryNo}>
-                            {country.countryName}
-                        </ListGroup.Item>
+                        {searchList.map(country => (
+                            <ListGroup.Item key={country.countryNo}>
+                                {country.countryName}
+                            </ListGroup.Item>
                         ))}
                     </ListGroup>
                 </div>
