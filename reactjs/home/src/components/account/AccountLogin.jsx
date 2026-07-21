b7ddc844851811ba257f7b91e374208a7c3e3187
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-07-21 12:00:17 +0900

    갱신

--- reactjs/home/src/components/account/AccountLogin.jsx
+++ reactjs/home/src/components/account/AccountLogin.jsx
@@ -35,7 +35,9 @@ export default function AccountLogin(){
     const navigate = useNavigate();
 
     //로그인
-    const sendLogin = useCallback(async ()=>{
+    const sendLogin = useCallback(async (e)=>{
+        e.preventDefault(); // 새로고침 방지
+
         //미입력 시 차단
         if(account.accountId === "" && account.accountPassword === ""){
             await Swal.fire("모든정보를 입력하세요");
@@ -53,35 +55,37 @@ export default function AccountLogin(){
             //로그인 실패
             await Swal.fire("정보가 일치하지 않습니다");
         }
-    },[account]);
+    },[account,loginAction, navigate]);
 
 
     return(<>
         <Jumbotron title="회원 로그인" content="로그인을 위한 정보를 입력해주세요"/>
 
-        <Row className="mt-4">
-            <Form.Label column sm={3}>아이디</Form.Label>
-            <Col sm={9}>
-                <Form.Control type="text" name="accountId" value={account.accountId}
-                    onChange={changeStringValue} placeholder="User ID"/>
-            </Col>
-        </Row>
-        <Row className="mt-4">
-            <Form.Label column sm={3}>비밀번호</Form.Label>
-            <Col sm={9}>
-                <Form.Control type="password" name="accountPassword" value={account.accountPassword}
-                    onChange={changeStringValue} placeholder="User Password"/>
-            </Col>
-        </Row>
+        <Form onSubmit={sendLogin}>
+            <Row className="mt-4">
+                <Form.Label column sm={3}>아이디</Form.Label>
+                <Col sm={9}>
+                    <Form.Control type="text" name="accountId" value={account.accountId}
+                        onChange={changeStringValue} placeholder="User ID"/>
+                </Col>
+            </Row>
+            <Row className="mt-4">
+                <Form.Label column sm={3}>비밀번호</Form.Label>
+                <Col sm={9}>
+                    <Form.Control type="password" name="accountPassword" value={account.accountPassword}
+                        onChange={changeStringValue} placeholder="User Password"/>
+                </Col>
+            </Row>
 
-        <Row className="mt-5">
-            <Col className="text-end">
-                <Button type="button" variant="success" className="w-100" 
-                    onClick={sendLogin}>
-                    <FaRightToBracket/>
-                    <span className="ms-2">로그인</span>
-                </Button>
-            </Col>
-        </Row>
+            <Row className="mt-5">
+                <Col className="text-end">
+                    {/* 2. 버튼 타입을 submit으로 변경 (onClick 제거 후 Form의 onSubmit이 처리) */}
+                    <Button type="submit" variant="success" className="w-100">
+                        <FaRightToBracket/>
+                        <span className="ms-2">로그인</span>
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
     </>)
 }
--- reactjs/home/src/templates/Menu.jsx
+++ reactjs/home/src/templates/Menu.jsx
@@ -10,6 +10,7 @@ import { RESET } from "jotai/utils";
 import { isLoginState, isAdminState } from "@utils/storage";
 import { logoutActionState } from "@utils/storage";
 import axios from "axios";
+import { loginActionState } from "../utils/storage";
 
 export default function Menu() {
     //메뉴에서는 로그인 상태 데이터가 필요하다
@@ -20,6 +21,7 @@ export default function Menu() {
     const isLogin = useAtomValue( isLoginState);
     const isAdmin = useAtomValue(isAdminState);
 
+    const loginAction = useSetAtom(loginActionState);
     const logoutAction = useSetAtom(logoutActionState);
 
     //서버에 로그아웃 요청 및 Jotai 저장소 초기화 요청을 수행하는 함수
@@ -35,6 +37,18 @@ export default function Menu() {
         }
     },[]);
 
+    //토큰 갱신 요청을 보내는 연습용 함수
+    const refresh = useCallback(async ()=>{
+        try{
+            const {data} = await axios.post("/service/auth/refresh");
+            //갱신이 된 경우(200 ok)
+            loginAction(data);
+        }
+        catch(e){
+            //갱신이 안된 경우(401 unauthorized)
+            logoutAction();
+        }
+    }, []);
     return (<>
         <Navbar expand="md" className="bg-body-tertiary sticky-top"
                     bg="dark" data-bs-theme="dark">
@@ -81,6 +95,9 @@ export default function Menu() {
                         <Nav.Link as={Link} to="/account/join">회원가입</Nav.Link>
                         <Nav.Link as={Link} to="/account/login">로그인</Nav.Link>
                         </>) }
+
+                        {/* 연습용 Refresh 버튼 (향후 삭제가 필요) */}
+                        <Nav.Link onClick={refresh}>갱신(Refresh)</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
