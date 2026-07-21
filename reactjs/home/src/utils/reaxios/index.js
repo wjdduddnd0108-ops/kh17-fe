cf029953d0d7e0a0eea2f7374c58d3ceabb41e6f
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-07-21 17:48:26 +0900

    로그인 완료

--- reactjs/home/src/utils/reaxios/index.js
+++ reactjs/home/src/utils/reaxios/index.js
@@ -30,26 +30,55 @@ export const  apiClient = axios.create({
 //- axios에는 interceptor(감시도구)라는 기능이 존재 
 //- axios 공식 사이트에서 제공하는 interceptor 구문을 가져다가 수정
 
-// Add a request interceptor
-apiClient.interceptors.request.use(
-  function (config) {
-    console.log("API 요청 발송 전", config);
-    return config;
-  },
-  function (error) {
-    console.log("API 요청 에러 발생", error);
-    return Promise.reject(error);
+// 응답에 대한 인터셉터
+apiClient.interceptors.response.use(
+  response=>response,
+  //요청이 실패한 경우만 분석해서 재작업을 지시
+ async function (error) {
+    // console.log(error?.response?.status);
+    if(error?.response?.status !== 401){
+        //통과
+        return Promise.reject(error);
+    }
+
+    //401인 상황 (=나는 로그인되어있다고 생각하는데 서버가 아니라고 하는 상황)
+    //-> Refresh로 요청을 보내서 나온 결과로 갈아끼워서 응답을 완수시킨다
+    // console.log(error.config);//원래 하려고 하던 요청정보(에러가 나서 못했음)
+    const originalRequest = error.config;
+    if(!originalRequest) {
+        return Promise.reject(error);//통과
+    }
+
+    //최초 요청인 겨우는 제외하고는 모두 통과시켜서 재요청이 발생하지 않도록 한다
+    if(originalRequest._retry){//표식이 존재한다면
+        console.log("retry 표식을 발견함");
+        moveToLoginPage();
+        return Promise.reject(error);//차단해
+    }
+
+    originalRequest._retry = true;
+    console.log("retry 표식을 남겼음");
+
+    console.log("액세스 토큰 만료됨 -> 갱신 요청 시작");
+    try{
+        const {data} = await authClient.post("/refresh")
+        //현재 화면은 로그인상태이므로 갱신이 필요하지 않음(필요하다면 해도 됨)
+        return apiClient(originalRequest);//apiClient에 원래 요청을 다시보낸 결과를 반환
+    }
+    catch(refreshError){
+        //로그인 페이지로 강제이동(리액트스럽게는 어려움)
+        moveToLoginPage();
+        return Promise.reject(refreshError);//새로운 에러로 갈아끼우고 통과시켜
+    }
+
+
   }
 );
 
-// Add a response interceptor
-apiClient.interceptors.response.use(
-  function (response) {
-    console.log("API 응답 성공", response);
-    return response;
-  },
-  function (error) {
-    console.log("API 응답 오류", error);
-    return Promise.reject(error);
-  }
-);
+//로그인 페이지로 이동하는 함수
+//- origin 제외하고 /부터 작성
+//- HashRouter는 처리가 안됨
+function moveToLoginPage(){
+    const url = "/account/login"
+    window.location.replace(url);
+}
--- reactjs/home/src/utils/storage/index.js
+++ reactjs/home/src/utils/storage/index.js
@@ -26,8 +26,8 @@ export const countState = atom(0);
 //- 객체 데이터를 저장하면서 localStorage, sessionStorage를 선택하고싶다면 직렬화 도구를 직접 생성해야함
 const localStorageWrapper = createJSONStorage(()=>window.localStorage);
 const sessionStorageWrapper = createJSONStorage(()=>window.sessionStorage);
-// export const loginUserState = atomWithStorage("loginUserState",null, localStorageWrapper);
-export const loginUserState = atomWithStorage("loginUserState",null, sessionStorageWrapper);
+export const loginUserState = atomWithStorage("loginUserState",null, localStorageWrapper);
+// export const loginUserState = atomWithStorage("loginUserState",null, sessionStorageWrapper);
 
 // 파생 atom - 다른 atom을 이용해서 계산을 처리한 결과를 만들어내는 atom (=useMemo 훅)
 // 생성방법 - atom(초기값)이 아니라 atom(GETTER, SETTER) 중 필요한걸 넣어서 처리하도록 구현
