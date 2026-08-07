9bc68d1bd812682e72297c4f80455958a8052326
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-08-07 11:06:56 +0900

    부분취소 구현

--- reactjs/home/src/components/pay/v2/KakaopayBuyDetailVersion2.jsx
+++ reactjs/home/src/components/pay/v2/KakaopayBuyDetailVersion2.jsx
@@ -6,6 +6,7 @@ import { ClockLoader } from "react-spinners";
 import { Badge, Button, Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import NoImage from "@assets/images/no-image.png";
 import { MdSubdirectoryArrowRight } from "react-icons/md";
+import Swal from 'sweetalert2'
 
 import dayjs from "dayjs";
 
@@ -15,6 +16,7 @@ dayjs.extend(relativeTime);
 //한국어로 설정
 import "dayjs/locale/ko";
 import { FaXmark } from "react-icons/fa6";
+import { toast } from "react-toastify";
 dayjs.locale("ko");
 
 export default function KakaopayBuyDetailVersion2() {
@@ -26,10 +28,10 @@ export default function KakaopayBuyDetailVersion2() {
     const [details, setDetails] = useState(null);
     const [payResponse, setPayResponse] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         loadData();
     }, []);
-    const loadData = useCallback(async ()=>{
+    const loadData = useCallback(async () => {
         const { data } = await apiClient.get(`/purchase/heavy/${purchaseNo}`);
         const { purchase, details, payResponse } = data;
         setPurchase(purchase);
@@ -39,8 +41,8 @@ export default function KakaopayBuyDetailVersion2() {
     }, []);
 
     //상품 개수까지 고려한 결제금액 계산
-    const calculateTotalPrice = useCallback((detail)=>{
-        if(!detail) throw "detail 없음";
+    const calculateTotalPrice = useCallback((detail) => {
+        if (!detail) throw "detail 없음";
 
         const { purchaseDetailQty, purchaseDetailPrice } = detail;
         const total = purchaseDetailPrice * purchaseDetailQty;
@@ -48,19 +50,79 @@ export default function KakaopayBuyDetailVersion2() {
     }, []);
 
     //memo
-    const withInPeriod = useMemo(()=>{
-        if(purchase === null) return false;
+    const withInPeriod = useMemo(() => {
+        if (purchase === null) return false;
         return dayjs().diff(purchase.purchaseCtime, 'day', false) <= 7;
     }, [purchase]);
 
-    if(purchase === null || details === null || payResponse === null) {
+    //전체 취소
+    const cancelAll = useCallback(async () => {
+        //확인창
+        try{
+            const result = await Swal.fire({
+                title: "결제를 취소하시겠습니까?",
+                text:"취소한 결제는 다시 복구할 수 없습니다",
+                icon: "warning",
+                confirmButtonText: "네, 취소하겠습니다",
+                cancelButtonText: "아니오, 취소하지 않겠습니다",
+                showCancelButton: true,
+                confirmButtonColor: "#d63031",
+                cancelButtonColor: "#b2bec3"
+            });
+            if(result.isConfirmed === false) return;
+    
+            const { data } = await apiClient.delete(`/purchase/cancelAll/${purchaseNo}`);
+            toast.success("결제가 취소되었습니다");
+
+            //화면 갱신 처리
+            // loadData();//뒷작업이 동시에 실행
+            await loadData();//뒷작업이 순차적으로 실행(async 함수 내에서 다른 async 함수를 부를 때)
+        }
+        catch(e){
+            toast.error("일시적인 오류입니다.\n잠시 후 다시 시도해주세요.");
+        }
+
+    }, []);
+
+    //항목취소
+    const cancelUnit = useCallback(async (detail) => {
+        //확인창
+        try{
+            const result = await Swal.fire({
+                title: "해당 상품의 구매를 취소하시겠습니까?",
+                text:"취소한 구매는 다시 복구할 수 없습니다",
+                icon: "warning",
+                confirmButtonText: "네, 취소하겠습니다",
+                cancelButtonText: "아니오, 취소하지 않겠습니다",
+                showCancelButton: true,
+                confirmButtonColor: "#d63031",
+                cancelButtonColor: "#b2bec3"
+            });
+            if(result.isConfirmed === false) return;
+    
+            const { data } = await apiClient.delete(
+                `/purchase/cancelUnit/${detail.purchaseDetailNo}`
+            );
+            toast.success("결제가 취소되었습니다");
+
+            //화면 갱신 처리
+            // loadData();//뒷작업이 동시에 실행
+            await loadData();//뒷작업이 순차적으로 실행(async 함수 내에서 다른 async 함수를 부를 때)
+        }
+        catch(e){
+            toast.error("일시적인 오류입니다.\n잠시 후 다시 시도해주세요.");
+        }
+
+    }, []);
+
+    if (purchase === null || details === null || payResponse === null) {
         return (<>
-            <Jumbotron title="상품 결제 상세" content="결제 내역을 불러오는 중입니다..."/>
+            <Jumbotron title="상품 결제 상세" content="결제 내역을 불러오는 중입니다..." />
 
             <Row className="mt-5">
                 <Col>
                     <div className="d-flex justify-content-center align-items-center">
-                        <ClockLoader size={100}/>
+                        <ClockLoader size={100} />
                     </div>
                 </Col>
             </Row>
@@ -68,7 +130,7 @@ export default function KakaopayBuyDetailVersion2() {
     }
 
     return (<>
-        <Jumbotron title="상품 결제 상세" content="PG사와 연동된 결제 정보 내역입니다"/>
+        <Jumbotron title="상품 결제 상세" content="PG사와 연동된 결제 정보 내역입니다" />
 
         {/* 결제 대표 정보(purchase) */}
         <Row className="mt-2">
@@ -117,80 +179,81 @@ export default function KakaopayBuyDetailVersion2() {
         </Row>
 
         {/* 전체 취소 버튼 */}
-        { (withInPeriod && purchase.purchaseRemain > 0) && (
-        <Row className="mt-4 text-end">
-            <Col>
-                <Button variant="danger" size="lg">
-                    <FaXmark/>
-                    <span className="ms-2">현재 구매내역 취소하기</span>
-                </Button>
-            </Col>
-        </Row>
-        ) }
+        {(withInPeriod && purchase.purchaseRemain > 0) && (
+            <Row className="mt-4 text-end">
+                <Col>
+                    <Button variant="danger" size="lg" onClick={cancelAll}>
+                        <FaXmark />
+                        <span className="ms-2">현재 구매내역 취소하기</span>
+                    </Button>
+                </Col>
+            </Row>
+        )}
 
         {/* 결제 상세 상품 정보 (purchase_detail) */}
-        <hr className="my-5"/>
+        <hr className="my-5" />
 
         <Row>
             <Col>
                 <ListGroup>
-                    {details.map(detail=>(
-                    <ListGroupItem key={detail.purchaseDetailNo} className="p-4">
-                        <div className="d-flex">
-                            {/* 상품 이미지(해결 필요) */}
-                            <div style={{width:100, height:100, overflow:"hidden"}}>
-                                <img src={NoImage} width={"100%"}/>
-                            </div>
+                    {details.map(detail => (
+                        <ListGroupItem key={detail.purchaseDetailNo} className="p-4">
+                            <div className="d-flex">
+                                {/* 상품 이미지(해결 필요) */}
+                                <div style={{ width: 100, height: 100, overflow: "hidden" }}>
+                                    <img src={NoImage} width={"100%"} />
+                                </div>
 
-                            {/* 상품 정보(스냅샷)와 구매 수량 */}
-                            <div className="flex-grow-1 ms-2">
-                                <h4 className="text-truncate">
-                                    <Link to={`/sale/detail/${detail.purchaseDetailItem}`}>
-                                        {detail.purchaseDetailName}
-                                    </Link>
-                                </h4>
-                                <div className="mt-4">
-                                    구매 수량 : {detail.purchaseDetailQty.toLocaleString()}개
-                                </div>
-                                <div className="mt-2">
-                                    구매 금액 : {calculateTotalPrice(detail)}원
-                                    &nbsp;
-                                    (개당 {detail.purchaseDetailPrice.toLocaleString()}원)
-                                </div>
-                                <div className="mt-2">
-                                    <Badge bg={
-                                        detail.purchaseDetailStatus === "승인" ? "success" : "danger"
-                                    }>
-                                        {detail.purchaseDetailStatus}
-                                    </Badge>
-                                </div>
-                                {/* 
+                                {/* 상품 정보(스냅샷)와 구매 수량 */}
+                                <div className="flex-grow-1 ms-2">
+                                    <h4 className="text-truncate">
+                                        <Link to={`/sale/detail/${detail.purchaseDetailItem}`}>
+                                            {detail.purchaseDetailName}
+                                        </Link>
+                                    </h4>
+                                    <div className="mt-4">
+                                        구매 수량 : {detail.purchaseDetailQty.toLocaleString()}개
+                                    </div>
+                                    <div className="mt-2">
+                                        구매 금액 : {calculateTotalPrice(detail)}원
+                                        &nbsp;
+                                        (개당 {detail.purchaseDetailPrice.toLocaleString()}원)
+                                    </div>
+                                    <div className="mt-2">
+                                        <Badge bg={
+                                            detail.purchaseDetailStatus === "승인" ? "success" : "danger"
+                                        }>
+                                            {detail.purchaseDetailStatus}
+                                        </Badge>
+                                    </div>
+                                    {/* 
                                     취소버튼 등장조건 
                                     1. 해당 상품 구매내역의 현재상태가 "승인"일 것
                                     2. 구매한지 일정 시간 이내일 것 (ex : 7일)
                                 */}
-                                { 
-                                    detail.purchaseDetailStatus === "승인"
-                                    &&
-                                    withInPeriod
-                                    && (
-                                <div className="mt-2 text-end">
-                                    <Button variant="danger" size="sm">
-                                        <FaXmark/>
-                                        <span className="ms-2">이 항목 취소하기</span>
-                                    </Button>
+                                    {
+                                        detail.purchaseDetailStatus === "승인"
+                                        &&
+                                        withInPeriod
+                                        && (
+                                            <div className="mt-2 text-end">
+                                                <Button variant="danger" size="sm"
+                                                    onClick={e=>cancelUnit(detail)}>
+                                                    <FaXmark />
+                                                    <span className="ms-2">이 항목 취소하기</span>
+                                                </Button>
+                                            </div>
+                                        )}
                                 </div>
-                                ) }
                             </div>
-                        </div>
-                    </ListGroupItem>                    
+                        </ListGroupItem>
                     ))}
                 </ListGroup>
             </Col>
         </Row>
 
         {/* 카카오페이 정보 */}
-        <hr className="my-5"/>
+        <hr className="my-5" />
         <Row className="mt-2">
             <Col sm={3} className="text-info fw-bold">지불방식</Col>
             <Col sm={9} className="text-secondary">
@@ -210,40 +273,40 @@ export default function KakaopayBuyDetailVersion2() {
             </Col>
         </Row>
         {payResponse.canceledAt !== null && (
-        <Row className="mt-2">
-            <Col sm={3} className="text-info fw-bold">결제 취소시간</Col>
-            <Col sm={9} className="text-secondary">
-                {dayjs(payResponse.canceledAt).format("YYYY년 M월 D일 dddd H시 m분 s초")}
-            </Col>
-        </Row>
-        ) }
+            <Row className="mt-2">
+                <Col sm={3} className="text-info fw-bold">결제 취소시간</Col>
+                <Col sm={9} className="text-secondary">
+                    {dayjs(payResponse.canceledAt).format("YYYY년 M월 D일 dddd H시 m분 s초")}
+                </Col>
+            </Row>
+        )}
         <Row className="mt-2">
             <Col sm={3} className="text-info fw-bold">금액상세</Col>
             <Col sm={9} className="text-secondary">
                 <div>
-                    총 
+                    총
                     <span className="text-info fw-bold mx-2">
                         {payResponse.amount.total.toLocaleString()}
                     </span>
                     원
                 </div>
                 <div className="ps-2">
-                    <MdSubdirectoryArrowRight/>
+                    <MdSubdirectoryArrowRight />
                     <span>
-                        상품가 
+                        상품가
                         <span className="text-info fw-bold mx-2">
                             {(payResponse.amount.total - payResponse.amount.vat).toLocaleString()}
-                        </span>    
+                        </span>
                         원
                     </span>
                 </div>
                 <div className="ps-2">
-                    <MdSubdirectoryArrowRight/>
+                    <MdSubdirectoryArrowRight />
                     <span>
-                        부가세 
+                        부가세
                         <span className="text-muted fw-bold mx-2">
                             {payResponse.amount.vat.toLocaleString()}
-                        </span>    
+                        </span>
                         원
                     </span>
                 </div>
@@ -254,23 +317,23 @@ export default function KakaopayBuyDetailVersion2() {
             <Col sm={3} className="text-info fw-bold">결제 상세</Col>
             <Col sm={9} className="text-secondary">
                 <ListGroup>
-                    {payResponse.paymentActionDetails.map((action, index)=>(
-                    <ListGroupItem key={index}>
-                        <div className="d-flex justify-content-between">
-                            <div>
-                                <Badge bg={
-                                    action.paymentActionType === "PAYMENT" ? "success" : "danger"
-                                }>{action.paymentActionType}</Badge>
+                    {payResponse.paymentActionDetails.map((action, index) => (
+                        <ListGroupItem key={index}>
+                            <div className="d-flex justify-content-between">
+                                <div>
+                                    <Badge bg={
+                                        action.paymentActionType === "PAYMENT" ? "success" : "danger"
+                                    }>{action.paymentActionType}</Badge>
 
-                                <span className="ms-2">
-                                    {action.amount.toLocaleString()} 원
-                                </span>
+                                    <span className="ms-2">
+                                        {action.amount.toLocaleString()} 원
+                                    </span>
+                                </div>
+                                <div>
+                                    {dayjs(action.approvedAt).format()}
+                                </div>
                             </div>
-                            <div>
-                                {dayjs(action.approvedAt).format()}
-                            </div>
-                        </div>
-                    </ListGroupItem>
+                        </ListGroupItem>
                     ))}
                 </ListGroup>
             </Col>
