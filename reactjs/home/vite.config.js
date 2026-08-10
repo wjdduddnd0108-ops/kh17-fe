f2d55301a79bd2e63e59ca22678e0366bcd0acca
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-08-10 12:36:19 +0900

    글로벌 이 window를 가리키도록 설정

--- reactjs/home/vite.config.js
+++ reactjs/home/vite.config.js
@@ -22,5 +22,9 @@ export default defineConfig({
       "@utils": path("./src/utils"),
       "@guard": path("./src/guard"),
     }
+  },
+  //라이브러리 구동에 필요한 값을 정의
+  define : {
+    global: "window",//global이라는 코드가 라이브러리에 있다면 window로 해석되도록 별칭 부여
   }
 })
