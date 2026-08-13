2784ece6b38701a3c0f6a5fb0164e3a1e02c78cf
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-08-13 12:26:12 +0900

    시험 프로젝트

---
+++ reactjs/khquiz/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Container from "react-bootstrap/esm/Container"
+
+export default function App(){
+
+  return (
+    <Container fluid>
+
+      
+    </Container>
+  )
+}
---
+++ reactjs/khquiz/src/templates/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header(){
+    return(<>
+        <div className="d-flex">
+            <div className="w-25 text-center"></div>
+            <div className="w-50 text-center">
+                <h1>KH정보교육원</h1>
+            </div>
+            <div className="w-25 text-center"></div>
+        </div>
+    </>)
+}
